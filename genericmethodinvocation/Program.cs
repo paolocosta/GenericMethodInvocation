5ed2e7f584de129a3fe33feb7af884a87a55086a
Paolo Costa <paolo.enrico.costa@gmail.com>
2014-12-20 19:36:32 +0100

    Versione iniziale 3

--- genericmethodinvocation/Program.cs
+++ genericmethodinvocation/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GenericMethodInvocation
 {
-    class Program//gzhgzgzuguzgz
+    class Program//gzhgzgzuguzgzbhbhbhjbhj
     {
         static void Main(string[] args)
         {
